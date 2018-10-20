ed0b385dd4bb1602943397b113c6120636a39425
Aaron Schlesinger <arschles@gmail.com>
2018-10-20 15:34:58 -0700

    adding stuff

--- .yadm/bootstrap-linux.sh
+++ .yadm/bootstrap-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install git mercurial zsh direnv xclip
+sudo apt-get install git mercurial zsh direnv xclip gnome-tweaks-tools net-tools
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
