2137f0ec1c4c78d45c21a370e80104a8019b3ae9
Aaron Schlesinger <aaron@ecomaz.net>
2019-10-11 20:57:28 -0700

    updates

--- .yadm/bootstrap-mac.sh
+++ .yadm/bootstrap-mac.sh
@@ -14,6 +14,3 @@ curl https://sh.rustup.rs -sSf | sh
 # change to zsh and use oh-my-zsh
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# install vscode settings
-cat ./vscode.json > $HOME/Library/Application\ Support/Code/User/settings.json
