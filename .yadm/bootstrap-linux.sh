5a09728d1b2d03fabb1e6b84ea31ffdcd106ce41
Aaron Schlesinger <70865+arschles@users.noreply.github.com>
2020-03-11 16:53:00 -0700

    Update bootstrap-linux.sh

--- .yadm/bootstrap-linux.sh
+++ .yadm/bootstrap-linux.sh
@@ -3,8 +3,9 @@
 sudo apt-get update
 sudo apt-add-repository ppa:obsproject/obs-studio
 sudo apt-get update
-sudo apt-get install ffmpeg obs-studio git mercurial zsh direnv xclip gnome-tweaks-tools net-tools
+sudo apt-get install ffmpeg git mercurial zsh direnv xclip gnome-tweaks-tools net-tools
 sudo apt install byobu golang direnv
+touch ./aliases-linux.sh
 cp ./aliases-linux.sh ~/.aliases
 source ~/.aliases
 chsh -s /bin/zsh
@@ -12,13 +13,3 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 
 
 curl https://sh.rustup.rs -sSf | sh
-
-sudo apt install gnome-tweaks gnome-shell-extensions dconf-editor gnome-weather kupfer
-sudo snap install communitheme
-sudo snap install vlc
-
-sudo snap install slack
-sudo snap install vscode --classic
-
-# set up the dock
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'firefox_dev.desktop', 'slack_slack.desktop', 'code.desktop', 'org.gnome.Terminal.desktop', 'gnome-control-center.desktop', 'software-properties-gtk.desktop', 'org.gnome.Software.desktop']"
