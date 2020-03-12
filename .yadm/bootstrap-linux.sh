bae337737d4106efdd1279dc8effbc65856ef642
Aaron Schlesinger <70865+arschles@users.noreply.github.com>
2020-03-11 17:01:13 -0700

    Update bootstrap-linux.sh

--- .yadm/bootstrap-linux.sh
+++ .yadm/bootstrap-linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Howdy! Welcome to the Linux bootstrap script"
 sudo apt-get update
 sudo apt-add-repository ppa:obsproject/obs-studio
 sudo apt-get update
@@ -8,8 +9,12 @@ sudo apt install byobu golang direnv
 touch ./aliases-linux.sh
 cp ./aliases-linux.sh ~/.aliases
 source ~/.aliases
-chsh -s /bin/zsh
+
+echo "Changing shells to zsh"
+chsh -s $(which zsh)
+
+echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-
+echo "Installing rust"
 curl https://sh.rustup.rs -sSf | sh
