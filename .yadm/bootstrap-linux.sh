132555955d8f136f5e990acbd1d0ae2b0aed3941
Aaron Schlesinger <70865+arschles@users.noreply.github.com>
2020-03-25 16:02:48 -0700

    Update bootstrap-linux.sh

--- .yadm/bootstrap-linux.sh
+++ .yadm/bootstrap-linux.sh
@@ -9,7 +9,7 @@ fi
 
 echo "Howdy! Welcome to the Linux bootstrap script"
 sudo apt update
-sudo apt install ffmpeg git mercurial zsh direnv xclip net-tools curl golang byobu direnv zsh curl
+sudo apt install ffmpeg git mercurial zsh direnv xclip net-tools curl golang byobu direnv zsh curl wget
 
 echo "Installing rust"
 curl https://sh.rustup.rs -sSf | sh
