7eecfe27593ef9065f75d32f20412bf82789b9c2
Aaron Schlesinger <70865+arschles@users.noreply.github.com>
2020-04-29 11:53:27 -0700

    Update install-vso.sh

--- install-vso.sh
+++ install-vso.sh
@@ -2,3 +2,11 @@
 
 export DEBIAN_FRONTEND=noninteractive
 ./.yadm/bootstrap-linux.sh
+
+cp ~/.yadm/zshrc ~/.zshrc
+touch ./aliases-linux.sh
+cp ~/.yadm/aliases-linux ~/.aliases
+source ~/.aliases
+source ~/.zshrc
+touch ~/.zprofile
+cp ~/.yadm/zprofile ~/.zprofile
