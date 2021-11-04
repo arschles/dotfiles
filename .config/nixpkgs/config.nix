1d0b592da60b6cbd59b3fc45292e4237ab9294cc
Aaron <aaron@ecomaz.net>
2021-11-04 20:33:38 +0000

    adding nix config
    Signed-off-by: Aaron <aaron@ecomaz.net>

---
+++ .config/nixpkgs/config.nix
@@ -0,0 +1,50 @@
+{
+    packageOverrides = pkgs: with pkgs; {
+        myPackages = pkgs.buildEnv {
+            name = "ecomaz-packages";
+            paths = [
+                yadm
+                mage
+                du-dust
+                azure-cli
+                direnv
+                git
+                gh
+                hugo
+                kubernetes-helm
+                jq
+                # python39
+                # cargo
+                # rustup
+                wget
+                dog
+                curl
+                zsh
+                nano
+                nodejs
+                hivemind
+                overmind
+                bat
+                exa
+                lsd
+                ripgrep
+                duf
+                broot
+                fzf
+                tealdeer
+                mdbook
+                curlie
+                bc
+                coreutils
+                gdb
+                ffmpeg
+                emscripten
+                silver-searcher
+                docker
+                # docker-gc
+                # docker-ls
+                delta
+            ];
+        };
+    };
+}
