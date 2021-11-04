1c7e9900004e1df404d787589b17e698402fc035
Aaron Schlesinger <70865+arschles@users.noreply.github.com>
2021-11-04 13:44:29 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+WHO=$(whoami)
+if [ "${WHO}" = "root" ]
+then
+    echo "You're root! Don't be root. Create a new user (useradd -m USERNAME) and then run this again"
+    exit
+fi
+
 export DEBIAN_FRONTEND=noninteractive
-./.yadm/bootstrap-linux.sh
+
+echo "Howdy! Welcome to the Install Script!"
+
+echo "Installing bin tool"
+curl -o ./bin -L https://github.com/marcosnils/bin/releases/download/v0.8.0/bin_0.8.0_Linux_x86_64
+./bin install github.com/marcosnils/bin
+rm ./bin
 
 cp ~/.yadm/zshrc ~/.zshrc
 touch ./aliases-linux.sh
 cp ~/.yadm/aliases-linux ~/.aliases
+cp ~/.yadm/zprofile ~/.zprofile
 source ~/.aliases
 source ~/.zshrc
 touch ~/.zprofile
-cp ~/.yadm/zprofile ~/.zprofile
