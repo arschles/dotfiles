222490d01abffaf0b900c7fe1c2eb2d9236200c9
Aaron Schlesinger <aaron@ecomaz.net>
2021-11-04 21:33:32 +0000

    uncommenting all
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -3,50 +3,49 @@
         myPackages = pkgs.buildEnv {
             name = "ecomaz-packages";
             paths = [
-                # yadm
-                # mage
-                # du-dust
-                # azure-cli
-                # direnv
-                # git
-                # tmux
-                # gh
-                # hugo
-                # kubernetes-helm
-                # jq
-                # python39
-                # cargo
-                # rustup
-                # wget
-                # dog
-                # curl
-                # zsh
-                # nano
-                # nodejs
-                # hivemind
-                # overmind
-                # bat
-                # exa
-                # lsd
-                # ripgrep
-                # duf
-                # broot
-                # fzf
-                # tealdeer
-                # mdbook
-                # curlie
-                # bc
-                # coreutils
-                # gdb
-                # ffmpeg
-                # emscripten
-                # silver-searcher
-                # docker
-                # docker-gc
-                # docker-ls
-                # delta
-                # starship
-                # go
+                yadm
+                mage
+                du-dust
+                azure-cli
+                direnv
+                git
+                tmux
+                gh
+                hugo
+                kubernetes-helm
+                jq
+                python39
+                cargo
+                rustup
+                wget
+                dog
+                curl
+                zsh
+                nano
+                nodejs
+                hivemind
+                overmind
+                bat
+                exa
+                lsd
+                ripgrep
+                duf
+                broot
+                fzf
+                tealdeer
+                mdbook
+                curlie
+                coreutils
+                gdb
+                ffmpeg
+                emscripten
+                silver-searcher
+                docker
+                docker-gc
+                docker-ls
+                delta
+                starship
+                go
                 golangci-lint
                 caddy
                 k9s
